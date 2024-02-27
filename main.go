4aca28d4cf24c09594e09a5471deb8739d573196
Meece <meece.code@gmail.com>
2024-02-27 13:07:09 +0200

    Add game loop

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strings"
 )
+
+var points int
 
 func main() {
 	file, err := os.Open("problems.csv")
@@ -15,13 +19,37 @@ func main() {
 	defer file.Close()
 
 	reader := csv.NewReader(file)
-
-	for {
-		record, err := reader.Read()
-		if err != nil {
-			break // Stop at EOF or any error
-		}
-		// Print each record
-		fmt.Println(record)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
 	}
+
+	startGame(records)
+
+	fmt.Printf("%d/%d Correct", points, len(records))
+}
+
+func startGame(records [][]string) {
+	for i, record := range records {
+
+		fmt.Printf("Question %d\n%s\n", i+1, record[0])
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Your Answer: ")
+		userAnswer, _ := reader.ReadString('\n')
+		userAnswer = strings.TrimSpace(userAnswer)
+
+		e := evaluateAnswer(record[1], userAnswer)
+		fmt.Println(e)
+		fmt.Println("---------------")
+	}
+}
+
+func evaluateAnswer(testAnswer string, userAnswer string) string {
+	if testAnswer == userAnswer {
+		points += 1
+		return "Correct"
+	}
+
+	return "Wrong"
 }
